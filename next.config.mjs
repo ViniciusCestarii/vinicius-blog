77c3bd225977e519f24a7097787b7d1a3e38c3cb
Vinicius Cestari <viniciuscestari01@gmail.com>
2024-09-15 17:29:32 -0300

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
+import createMDX from '@next/mdx'
+ 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+}
+ 
+const withMDX = createMDX({
+})
+ 
+export default withMDX(nextConfig)
---
+++ src/app/test/page.tsx
@@ -0,0 +1,5 @@
+import Welcome from '@/app/test/page-content.mdx'
+
+export default function Page() {
+  return <Welcome />
+}
