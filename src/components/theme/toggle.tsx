ba2a7bafc07b5c59b70e83679ea970f00f0a08f4
ViniciusCestarii <viniciuscestari01@gmail.com>
2024-09-19 09:51:09 -0300

    feat: update theme toggle

--- src/components/theme/toggle.tsx
+++ src/components/theme/toggle.tsx
@@ -5,36 +5,24 @@ import { Moon, Sun } from 'lucide-react'
 import { useTheme } from 'next-themes'
 
 import { Button } from '@/components/ui/button'
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu'
 
 export function ThemeToggle() {
-  const { setTheme } = useTheme()
+  const { setTheme, resolvedTheme } = useTheme()
+
+  const handleThemeChange = () => {
+    setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')
+  }
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon" className="group">
-          <Sun className="h-[1.2rem] w-[1.2rem] group-hover:rotate-90 scale-100 transition-all dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] group-hover:rotate-45 -rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme('light')}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme('dark')}>
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme('system')}>
-          System
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <Button
+      variant="ghost"
+      size="icon"
+      className="group"
+      onClick={handleThemeChange}
+    >
+      <Sun className="h-[1.2rem] w-[1.2rem] group-hover:rotate-90 transition-all block dark:hidden" />
+      <Moon className="h-[1.2rem] w-[1.2rem] group-hover:rotate-45 -rotate-90 transition-all dark:rotate-0 hidden dark:block p-[0.001rem]" />
+      <span className="sr-only">Toggle theme</span>
+    </Button>
   )
 }
