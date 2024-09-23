a7405ef1ac0ac8e2fa2c0c404428090277ecd671
ViniciusCestarii <viniciuscestari01@gmail.com>
2024-09-23 14:23:09 -0300

    feat: add botton pading for blog list

--- src/app/(home)/blog-list.tsx
+++ src/app/(home)/blog-list.tsx
@@ -7,7 +7,7 @@ interface BlogListProps {
 
 const BlogList = ({ posts }: BlogListProps) => {
   return (
-    <div className="flex flex-col gap-8 items-center sm:items-start">
+    <div className="flex flex-col gap-8 items-center sm:items-start pb-8">
       <BlogListHeader />
       {posts.length > 0 ? (
         <ul className="flex flex-col gap-10 w-full px-4">
