6b94eb815654132a65ef94b31c75ac4157058406
ViniciusCestarii <viniciuscestari01@gmail.com>
2024-09-25 14:37:26 -0300

    feat: improve edit post screen

--- src/app/(main)/blog/[slug]/edit-blog-dialog.tsx
+++ src/app/(main)/blog/[slug]/edit-blog-dialog.tsx
@@ -18,6 +18,11 @@ import {
   DialogTrigger,
 } from '@/components/ui/dialog'
 import { Input } from '@/components/ui/input'
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from '@/components/ui/resizable'
 import { Textarea } from '@/components/ui/textarea'
 import { Post } from '@/lib/blog/types'
 import { updatePostCommit } from '@/lib/github/blog'
@@ -72,82 +77,92 @@ const EditBlogDialogBase = ({ post }: EditBlogDialogProps) => {
       <DialogTrigger asChild>
         <Button>Edit post</Button>
       </DialogTrigger>
-      <DialogContent className="h-[90vh] p-4 max-w-screen-lg overflow-auto">
+      <DialogContent className="p-4 max-w-screen-2xl">
         <DialogHeader>
           <DialogTitle>Edit post</DialogTitle>
           <DialogDescription>
             Fill in the form below to edit the post
           </DialogDescription>
         </DialogHeader>
-        <Accordion type="single" collapsible className="w-full">
-          <AccordionItem value="item-1">
-            <AccordionTrigger>Edit metadata</AccordionTrigger>
-            <AccordionContent className="space-y-4 px-1">
-              <div>
-                <label htmlFor="edit-description">Description</label>
-                <Textarea
-                  id="edit-description"
-                  value={description}
-                  onChange={(e) => setDescription(e.target.value)}
-                />
-              </div>
-              <div className="flex flex-col sm:flex-row sm:items-center gap-2">
-                <div className="flex-1">
-                  <label htmlFor="edit-tags">Tags</label>
-                  <Input
-                    id="edit-tags"
-                    value={tags.join(',')}
-                    onChange={(e) =>
-                      setTags(
-                        e.target.value.split(',').map((tag) => tag.trim()),
-                      )
-                    }
-                  />
-                </div>
+        <div className="overflow-auto h-[80vh]">
+          <Accordion type="single" collapsible className="w-full">
+            <AccordionItem value="item-1">
+              <AccordionTrigger>Edit metadata</AccordionTrigger>
+              <AccordionContent className="space-y-4 px-1">
                 <div>
-                  <label htmlFor="edit-published-at">Published at</label>
-                  <Input
-                    id="edit-published-at"
-                    type="date"
-                    value={publishedAt}
-                    onChange={(e) => setPublishedAt(e.target.value)}
+                  <label htmlFor="edit-description">Description</label>
+                  <Textarea
+                    placeholder="A short description of the post"
+                    id="edit-description"
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
                   />
                 </div>
-                <Button
-                  className="mt-auto"
-                  variant={status === 'published' ? 'destructive' : 'default'}
-                  onClick={() =>
-                    status === 'published'
-                      ? setStatus('draft')
-                      : setStatus('published')
-                  }
-                >
-                  {status === 'published' ? 'Unpublish' : 'Publish'}
-                </Button>
-              </div>
-            </AccordionContent>
-          </AccordionItem>
-        </Accordion>
+                <div className="flex flex-col sm:flex-row sm:items-center gap-2">
+                  <div className="flex-1">
+                    <label htmlFor="edit-tags">Tags</label>
+                    <Input
+                      id="edit-tags"
+                      value={tags.join(',')}
+                      onChange={(e) =>
+                        setTags(
+                          e.target.value.split(',').map((tag) => tag.trim()),
+                        )
+                      }
+                    />
+                  </div>
+                  <div>
+                    <label htmlFor="edit-published-at">Published at</label>
+                    <Input
+                      id="edit-published-at"
+                      type="date"
+                      value={publishedAt}
+                      onChange={(e) => setPublishedAt(e.target.value)}
+                    />
+                  </div>
+                  <Button
+                    className="mt-auto"
+                    variant={status === 'published' ? 'destructive' : 'default'}
+                    onClick={() =>
+                      status === 'published'
+                        ? setStatus('draft')
+                        : setStatus('published')
+                    }
+                  >
+                    {status === 'published' ? 'Unpublish' : 'Publish'}
+                  </Button>
+                </div>
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
 
-        <div className="grid sm:grid-cols-3 gap-4 w-full">
-          <div>
-            <label htmlFor="edit-content">Content</label>
-            <Textarea
-              id="edit-content"
-              value={content}
-              onChange={(e) => setContent(e.target.value)}
-              className="min-h-96"
-            />
-          </div>
-          <article className="border-b sm:border-none sm:col-span-2 h-[70vh] overflow-auto article-body">
-            <ClientMdxViewer source={content} />
-          </article>
+          <ResizablePanelGroup
+            direction="horizontal"
+            className="max-h-[calc(80vh_-6rem)]"
+          >
+            <ResizablePanel defaultSize={60}>
+              <label htmlFor="edit-content">Content</label>
+              <Textarea
+                id="edit-content"
+                placeholder="Write your post here."
+                value={content}
+                onChange={(e) => setContent(e.target.value)}
+                className="min-h-96"
+              />
+            </ResizablePanel>
+            <ResizableHandle />
+            <ResizablePanel defaultSize={50}>
+              <article className="border-b sm:border-none h-full overflow-auto article-body">
+                <ClientMdxViewer source={content} />
+              </article>
+            </ResizablePanel>
+          </ResizablePanelGroup>
+          <DialogFooter>
+            <Button type="submit" disabled={isPending} onClick={updateBlogPost}>
+              Edit post
+            </Button>
+          </DialogFooter>
         </div>
-        <DialogFooter>
-          <Button type="submit" disabled={isPending} onClick={updateBlogPost}>
-            Edit post
-          </Button>
-        </DialogFooter>
       </DialogContent>
     </Dialog>
   )
