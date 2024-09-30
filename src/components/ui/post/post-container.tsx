76cd60af89c8cdc87306a32e89fc012d9a9865b9
ViniciusCestarii <viniciuscestari01@gmail.com>
2024-09-30 10:42:04 -0300

    feat: add padding bottom on post container

--- src/components/ui/post/post-container.tsx
+++ src/components/ui/post/post-container.tsx
@@ -1,5 +1,6 @@
 import { cn } from '@/lib/style/utils'
 
+const CONTAINER_STYLE = 'pb-16'
 const BASE_PROSE = 'prose dark:prose-invert text-foreground prose-sky'
 const HEADING_PROSE =
   'prose-headings:font-geist-mono prose-headings:tracking-tighter'
@@ -12,7 +13,10 @@ const PostContariner = ({
   ...props
 }: PostContarinerProps) => {
   return (
-    <article {...props} className={cn(BASE_PROSE, HEADING_PROSE, className)}>
+    <article
+      {...props}
+      className={cn(CONTAINER_STYLE, BASE_PROSE, HEADING_PROSE, className)}
+    >
       {children}
     </article>
   )
