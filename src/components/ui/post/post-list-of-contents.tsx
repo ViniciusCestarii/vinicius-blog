220831c8912185c4c517eb54233522f071ccf5a3
ViniciusCestarii <viniciuscestari01@gmail.com>
2024-10-07 17:31:48 -0300

    feat: add basic list of content tracker

--- src/components/ui/post/post-list-of-contents.tsx
+++ src/components/ui/post/post-list-of-contents.tsx
@@ -1,33 +1,59 @@
+'use client'
+
 import { getHeadings } from '@/lib/blog/utils'
-import { cn } from '@/lib/style/utils'
 import { ArrowUp } from 'lucide-react'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+import { cn } from '@/lib/style/utils'
 
 interface PostListOfContentsProps {
   content: string
 }
 
 const PostListOfContents = ({ content }: PostListOfContentsProps) => {
+  const [activeSlug, setActiveSlug] = useState<string | null>(null)
   const headings = getHeadings(content)
+
+  useEffect(() => {
+    const handleObserver = (entries: IntersectionObserverEntry[]) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          setActiveSlug(entry.target.id)
+        }
+      })
+    }
+
+    const observer = new IntersectionObserver(handleObserver, {
+      rootMargin: '0px 0px -80% 0px', // Detects heading entering the viewport
+    })
+
+    headings.forEach((heading) => {
+      const element = document.getElementById(heading.slug)
+      if (element) observer.observe(element)
+    })
+
+    return () => {
+      headings.forEach((heading) => {
+        const element = document.getElementById(heading.slug)
+        if (element) observer.unobserve(element)
+      })
+    }
+  }, [headings])
 
   return (
     <aside className="not-prose hidden xl:block xl:col-start-4 h-0 pt-8 sticky top-0 text-muted-foreground">
-      <h3 className="text-sm text-muted-foreground/70 uppercase">
+      <h3 className="text-sm text-muted-foreground/70 uppercase pl-2 border-l border-muted-foreground/50">
         On this page
       </h3>
       <ul>
         {headings.map((heading) => (
-          <li key={heading.text} data-spacing={heading.heading}>
-            <a
-              className={cn('hover:underline hover:text-foreground', {
-                'pl-4': heading.heading === 3,
-                'pl-8': heading.heading === 4,
-              })}
-              href={`#${heading.slug}`}
-            >
-              {heading.text}
-            </a>
-          </li>
+          <PostLink
+            key={heading.text}
+            heading={heading.heading}
+            text={heading.text}
+            slug={heading.slug}
+            isInView={activeSlug === heading.slug}
+          />
         ))}
       </ul>
       <div className="border-t border-muted-foreground/50 flex mt-4">
@@ -39,6 +65,38 @@ const PostListOfContents = ({ content }: PostListOfContentsProps) => {
         </a>
       </div>
     </aside>
+  )
+}
+
+interface PostLinkProps {
+  heading: number
+  text: string
+  slug: string
+  isInView: boolean
+}
+
+const PostLink = ({ heading, text, slug, isInView }: PostLinkProps) => {
+  return (
+    <li
+      data-spacing={heading}
+      className={cn(
+        'border-l border-muted-foreground/50 pl-2 transition-colors',
+        {
+          'border-primary border-l-2': isInView,
+        },
+      )}
+    >
+      <a
+        className={cn('hover:underline hover:text-foreground', {
+          'pl-4': heading === 3,
+          'pl-8': heading === 4,
+          '-ml-[1px]': isInView,
+        })}
+        href={`#${slug}`}
+      >
+        {text}
+      </a>
+    </li>
   )
 }
 
