289f0423fad3cda7eef4e31fac35bb67d5b286d9
ViniciusCestarii <viniciuscestari01@gmail.com>
2024-10-10 17:01:50 -0300

    feat: remove useless dependecies

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
-import createMDX from '@next/mdx'
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
 }
 
-const withMDX = createMDX({})
-
-export default withMDX(nextConfig)
+export default nextConfig
