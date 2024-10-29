7d668e0b9e1ee2a485f937ef3a1b516e5bf6df60
ViniciusCestarii <viniciuscestari01@gmail.com>
2024-10-29 14:14:10 -0300

    feat: improve search focus ring

--- src/app/(home)/post-search.tsx
+++ src/app/(home)/post-search.tsx
@@ -13,7 +13,7 @@ const PostSearch = () => {
   )
 
   return (
-    <div className="flex w-full items-center bg-card rounded-md border border-input">
+    <div className="flex w-full items-center bg-card rounded-md border border-input ring-ring ring-offset-background focus-within:ring-2 focus-within:ring-offset-2">
       <label htmlFor="post-search" className="flex-shrink-0 pl-2 pr-1">
         <span className="sr-only">Search for blog post</span>
         <Search />
@@ -23,7 +23,7 @@ const PostSearch = () => {
         id="post-search"
         type="search"
         placeholder="Search by title, description or tag"
-        className="w-full p-2 bg-card flex-1 border-none"
+        className="w-full p-2 bg-card flex-1 border-none focus-visible:ring-0"
         value={search}
         onChange={({ target }) =>
           !target.value ? setSearch(null) : setSearch(target.value)
