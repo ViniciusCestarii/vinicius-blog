122c7debe46437e3afa7bb50016972e4799b0330
ViniciusCestarii <viniciuscestari01@gmail.com>
2025-01-22 15:47:16 -0300

    feat: add dynamic year on footer

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -5,7 +5,9 @@ const Footer = () => {
   return (
     <footer className="flex justify-center z-10 border-t sticky top-0 backdrop-blur-sm bg-background/45">
       <div className="flex max-w-screen-xl flex-1 items-center text-center flex-col text-sm gap-2 p-2 ">
-        <span>© 2024 Vinicius Cestari. All rights reserved</span>
+        <span>
+          © {new Date().getFullYear()} Vinicius Cestari. All rights reserved
+        </span>
         <span>
           Published under{' '}
           <ExternalLink href="https://github.com/ViniciusCestarii/vinicius-blog/blob/main/LICENSE">
