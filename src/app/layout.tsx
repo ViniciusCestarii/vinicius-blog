098df30b056e8bb011efab9cdb68122cf1534274
ViniciusCestarii <viniciuscestari01@gmail.com>
2025-01-24 16:42:45 -0300

    feat: add noise background

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,7 +42,7 @@ export default async function RootLayout({
         >
           <QueryClientProvider>
             <AuthProvider>
-              <div className="px-4 page-layout">{children}</div>
+              <div className="px-4 page-layout bg-noise">{children}</div>
             </AuthProvider>
           </QueryClientProvider>
           <Toaster closeButton />
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -100,6 +100,9 @@ const config: Config = {
           '5': 'hsl(var(--chart-5))',
         },
       },
+      backgroundImage: {
+        noise: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='100%25' height='100%25'%3E%3Cfilter id='noiseFilter'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.65' numOctaves='3' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noiseFilter)' opacity='0.075'/%3E%3C/svg%3E")`,
+      },
       fontFamily: {
         'geist-sans': 'var(--font-geist-sans)',
         'geist-mono': 'var(--font-geist-mono)',
