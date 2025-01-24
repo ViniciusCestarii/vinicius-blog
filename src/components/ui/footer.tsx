30dadc83a879599b2d84d12b5c2710ff42db8491
ViniciusCestarii <viniciuscestari01@gmail.com>
2025-01-24 16:45:02 -0300

    feat: update footer

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -3,7 +3,7 @@ import GithubLink from './links/github-link'
 
 const Footer = () => {
   return (
-    <footer className="flex justify-center z-10 border-t sticky top-0 backdrop-blur-sm bg-background/45">
+    <footer className="flex justify-center border-t">
       <div className="flex max-w-screen-xl flex-1 items-center text-center flex-col text-sm gap-2 p-2 ">
         <span>
           © {new Date().getFullYear()} Vinicius Cestari. All rights reserved
