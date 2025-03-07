f20484a583c698689e826c431a3acb56a8135df9
ViniciusCestarii <viniciuscestari01@gmail.com>
2025-03-07 10:22:11 -0300

    feat: use slide page transition

--- src/components/ui/main-container.tsx
+++ src/components/ui/main-container.tsx
@@ -3,11 +3,10 @@ import React, { unstable_ViewTransition as ViewTransition } from 'react'
 interface MainContainerProps {
   children: React.ReactNode
 }
-// using the default behavior of ViewTransition
-// which is to fade in and out the children
+
 const MainContainer = ({ children }: MainContainerProps) => {
   return (
-    <ViewTransition>
+    <ViewTransition name="main-container">
       <main className="min-h-main page-layout">{children}</main>
     </ViewTransition>
   )
